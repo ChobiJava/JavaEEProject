6f3fc6464ba7fe73c70c2cce1bb98d27b84f60b8
Mattias <mattias_sonemar@hotmail.com>
2015-09-14 11:25:45 +0200

    added some more stuff to frontend

---
+++ javaeeproject-web/src/main/webapp/resources/javascript/javascript.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Chobii on 14/09/15.
+ */
+
+$(function() {
+    $(".ssso a:first").attr('href', "/studentView.xhtml");
+});
+
+$(".ssso a:first").onclick(function() {
+    window.location.href="studentView.xhtml";
+});
