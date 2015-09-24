b0033972fe374690d3198c86813100c414a770a7
Mattias <mattias_sonemar@hotmail.com>
2015-09-24 08:48:15 +0200

    removed named persistencecontext, removed unnedded css classes

--- javaeeproject-ejb/src/main/java/com/chobi/util/Resources.java
+++ javaeeproject-ejb/src/main/java/com/chobi/util/Resources.java
@@ -2,19 +2,13 @@ package com.chobi.util;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.logging.Logger;
 
 /**
  * Created by Chobii on 08/09/15.
  */
 public class Resources {
-
-    @Produces
-    @PersistenceContext
-    private EntityManager em;
-
+    
     @Produces
     public Logger produceLog(InjectionPoint injectionPoint) {
         return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
